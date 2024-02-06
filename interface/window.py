dcf8f6dc133e6f98a1db4eb564712469e5ab8882
pelleservan <pelle.servan@hotmail.fr>
2024-02-06 18:29:08 +0100

    start interface

---
+++ interface/window.py
@@ -0,0 +1,93 @@
+import tkinter as tk
+
+from cypher.cypher import cypher, inverse_cypher
+from config import mix, imix
+
+class Window(tk.Tk):
+    def __init__(self):
+        super().__init__()
+
+        self.title("AES")
+
+        self.bg_color = '#3D3B40'
+        self.front_color = '#525CEB'
+
+
+        self.configure(bg=self.bg_color)
+
+        self.initial_msg = ''
+        self.encryption_key = ''
+        self.crypted_msg = tk.StringVar()
+        self.crypted_msg.set("Encrypted message : ")
+        self.keys_array = []
+
+        self.generate_container()
+
+    def generate_container(self):
+
+        tk.Label(self, text=f'AES algotihm', fg=self.front_color, bg=self.bg_color, font=("Arial", 30)).grid(row=0, column=0, sticky='ew')
+
+        self.container = tk.Frame(self, bg=self.bg_color)
+        self.container.grid(row=1, column=0, sticky='nsew', padx=10, pady=10)
+
+        ### Encrypte
+        self.encrypt_container = tk.Frame(self.container, bg=self.front_color)
+        self.encrypt_container.grid(row=0, column=0, sticky='nsew')
+
+        tk.Label(self.encrypt_container, text=f'Encrypt a message :', bg=self.front_color, font=("Arial", 20)).grid(row=0, column=0, sticky='w')
+
+        tk.Label(self.encrypt_container, text=f'Initial message : ', bg=self.front_color, font=("Arial", 15)).grid(row=1, column=0, sticky='w')
+
+        self.initial_msg_entry = tk.Entry(self.encrypt_container, width=40, bg=self.bg_color, highlightbackground=self.front_color)
+        self.initial_msg_entry.insert(0, '00112233445566778899aabbccddeeff')
+        self.initial_msg_entry.grid(row=1, column=1, sticky='ew')
+
+        tk.Label(self.encrypt_container, text='Encryption key : ', bg=self.front_color, font=("Arial", 15)).grid(row=2, column=0, sticky='w')
+
+        self.encryption_key_entry = tk.Entry(self.encrypt_container, width=40, bg=self.bg_color, highlightbackground=self.front_color)
+        self.encryption_key_entry.insert(2, '000102030405060708090a0b0c0d0e0f')
+        self.encryption_key_entry.grid(row=2, column=1, sticky='ew')
+
+        tk.Button(self.encrypt_container, text=f'Crypt message', command=lambda:self.run_cypher(), bg=self.front_color, highlightbackground=self.front_color).grid(row=1, column=2, rowspan=2, sticky='nsew')
+
+        tk.Label(self.encrypt_container, textvariable=self.crypted_msg, bg=self.front_color, font=("Arial", 15)).grid(row=3, column=0, columnspan=3, sticky='w')
+
+        ### Decrypte
+        self.decrypt_container = tk.Frame(self.container, bg=self.front_color)
+        self.decrypt_container.grid(row=1, column=0, sticky='nsew', pady=10)
+
+        tk.Label(self.decrypt_container, text=f'Decrypt a message :', bg=self.front_color, font=("Arial", 20)).grid(row=0, column=0, sticky='w')
+
+        tk.Label(self.decrypt_container, text=f'Encrypted message : ', bg=self.front_color, font=("Arial", 15)).grid(row=1, column=0, sticky='w')
+
+        self.encrypted_msg_entry = tk.Entry(self.decrypt_container, width=40, bg=self.bg_color, highlightbackground=self.front_color)
+        self.encrypted_msg_entry.insert(0, self.crypted_msg)
+        self.encrypted_msg_entry.grid(row=1, column=1, sticky='ew')
+
+        tk.Label(self.decrypt_container, text='Encryption key : ', bg=self.front_color, font=("Arial", 15)).grid(row=2, column=0, sticky='w')
+
+        self.decryption_key_entry = tk.Entry(self.decrypt_container, width=40, bg=self.bg_color, highlightbackground=self.front_color)
+        self.decryption_key_entry.insert(2, '000102030405060708090a0b0c0d0e0f')
+        self.decryption_key_entry.grid(row=2, column=1, sticky='ew')
+
+        tk.Button(self.decrypt_container, text=f'Decrypt message', command=lambda:self.run_cypher(), bg=self.front_color, highlightbackground=self.front_color).grid(row=1, column=2, rowspan=2, sticky='nsew')
+
+        tk.Label(self.decrypt_container, textvariable=self.crypted_msg, bg=self.front_color, font=("Arial", 15)).grid(row=3, column=0, columnspan=3, sticky='w')
+
+    def run_cypher(self):
+        self.initial_msg = self.initial_msg_entry.get()
+        self.encryption_key = self.encryption_key_entry.get()
+
+        if len(self.initial_msg) == 32 and len(self.encryption_key) == 32:
+            cypher_output = cypher(nb_round=10, initial_msg=self.initial_msg, chifrement_key=self.encryption_key, mix=mix)
+            self.crypted_msg.set(f'Encrypted message : {cypher_output[0]}')
+            self.keys_array = cypher_output[1]
+
+    def run_inverse_cypher(self):
+        self.initial_msg = self.initial_msg_entry.get()
+        self.encryption_key = self.encryption_key_entry.get()
+
+        if len(self.initial_msg) == 32 and len(self.encryption_key) == 32:
+            cypher_output = cypher(nb_round=10, initial_msg=self.initial_msg, chifrement_key=self.encryption_key, mix=mix)
+            self.crypted_msg.set(f'Encrypted message : {cypher_output[0]}')
+            self.keys_array = cypher_output[1]
--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-from cypher.cypher import cypher, inverse_cypher
-from config import mix, imix
+from interface.window import Window
 
 if __name__ == '__main__':
 
-    initial_msg =    '00112233445566778899aabbccddeeff'
-    chifrement_key = '000102030405060708090a0b0c0d0e0f'
-
-    output = cypher(nb_round=10, initial_msg=initial_msg, chifrement_key=chifrement_key, mix=mix)
-
-    print('\n\n')
-
-    inverse_cypher(nb_round=10, crypted_msg=output[0], keys=output[1], imix=imix)
+    app = Window()
+    app.mainloop()
